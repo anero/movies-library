040895d532ca1e120ca6532022af8060e70ad9f4
Diego Marcet <diego.marcet@tipit.net>
2012-09-19 20:36:14 -0300

    Added sorting to FolderListingProvider class

--- lib/folder_listing_provider.rb
+++ lib/folder_listing_provider.rb
@@ -6,14 +6,20 @@ module MoviesLibrary
 			@folder_path = folder_path
 		end
 
-		def get_folders
+		def get_folders(options={})
 			folder_path = Dir.entries(@folder_path).select do |entry|
 				File.directory? File.join(@folder_path, entry) and !(entry =='.' || entry == '..')
 			end
 
-			folder_path.collect do |fp|
+			folders = folder_path.collect do |fp|
 				MoviesLibrary::Models::Folder.new(File.join(@folder_path, fp))
 			end
+
+			if (options[:order_by])
+				folders.sort_by {|f| f.send(options[:order_by]) unless !f.respond_to?(options[:order_by])}
+			end
+
+			folders
 		end
 	end
 end
--- spec/folder_listing_provider_spec.rb
+++ spec/folder_listing_provider_spec.rb
@@ -4,12 +4,15 @@ require File.expand_path('../../lib/folder_listing_provider', __FILE__)
 describe MoviesLibrary::FolderListingProvider do
 	before(:all) do
 		Dir.mkdir(get_absolute_path_to_temp_resource_folder(''))
-		Dir.mkdir(get_absolute_path_to_temp_resource_folder('dir_1'))
-		Dir.mkdir(get_absolute_path_to_temp_resource_folder('dir_2'))
+		Dir.mkdir(get_absolute_path_to_temp_resource_folder('dir_z'))
+		Dir.mkdir(get_absolute_path_to_temp_resource_folder('dir_x'))
+		Dir.mkdir(get_absolute_path_to_temp_resource_folder('dir_y'))
 	end
+
 	after(:all) do
-		Dir.delete(get_absolute_path_to_temp_resource_folder('dir_1'))
-		Dir.delete(get_absolute_path_to_temp_resource_folder('dir_2'))
+		Dir.delete(get_absolute_path_to_temp_resource_folder('dir_z'))
+		Dir.delete(get_absolute_path_to_temp_resource_folder('dir_x'))
+		Dir.delete(get_absolute_path_to_temp_resource_folder('dir_y'))
 		Dir.delete(get_absolute_path_to_temp_resource_folder(''))
 	end
 
@@ -19,9 +22,30 @@ describe MoviesLibrary::FolderListingProvider do
 		folders = folder_listing_provider.get_folders
 
 		folders.should_not be_nil
-		folders.count.should == 2
-		folders.any? {|f| f.name == 'dir_1' }.should be_true
-		folders.any? {|f| f.name == 'dir_2' }.should be_true
+		folders.count.should == 3
+		folders.any? {|f| f.name == 'dir_x' }.should be_true
+		folders.any? {|f| f.name == 'dir_y' }.should be_true
+		folders.any? {|f| f.name == 'dir_z' }.should be_true
+	end
+
+	it "should sort folders list by creation date" do
+		folder_listing_provider = MoviesLibrary::FolderListingProvider.new(get_absolute_path_to_temp_resource_folder)
+
+		folders = folder_listing_provider.get_folders(:order_by => :creation_date)
+
+		folders[0].name == 'dir_z'
+		folders[1].name == 'dir_x'
+		folders[1].name == 'dir_y'
+	end
+
+	it "should sort folders list by name" do
+		folder_listing_provider = MoviesLibrary::FolderListingProvider.new(get_absolute_path_to_temp_resource_folder)
+
+		folders = folder_listing_provider.get_folders(:order_by => :name)
+
+		folders[0].name == 'dir_x'
+		folders[1].name == 'dir_y'
+		folders[1].name == 'dir_z'
 	end
 
 	private
