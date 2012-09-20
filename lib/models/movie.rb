cbddd2976219fffd1323df42631938b79e1f2126
Diego Marcet <diego.marcet@tipit.net>
2012-09-20 19:03:53 -0300

    Created Movie class

---
+++ lib/models/movie.rb
@@ -0,0 +1,7 @@
+module MoviesLibrary
+	module Models
+		class Movie
+			attr_accessor :title, :synopsis, :download_date, :has_subtitles, :download_complete
+		end
+	end
+end
---
+++ spec/models/movie_folder_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require File.expand_path('../../../lib/models/movie_folder', __FILE__)
+
+describe MoviesLibrary::Models::MovieFolder do
+	include MoviesLibrary::MovieFolderHelper
+
+end
